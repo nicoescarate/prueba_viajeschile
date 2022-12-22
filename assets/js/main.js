1e6b2b5cd92946429826dd22e88d13ff97fef59a
nicoescarate <51349736+nicoescarate@users.noreply.github.com>
2022-12-22 18:44:48 -0300

    Estilo
    Carga de datos de estilo para la página incluyendo la hoja de Javascript y jquery

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+
+    $('#enviarporcorreo').click(function(){
+        alert('El correo fue enviado correctamente')
+    
+    })
+    
+    $('[data-toggle="tooltip"]').tooltip()
+    
+    
+    })
